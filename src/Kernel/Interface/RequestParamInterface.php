f977b438319abf22b66d85ccff6880431cd57acc
jsogn <jbanxian@gmail.com>
2024-05-14 13:45:54 +0800

    移除参数类验证

--- src/Kernel/Interface/RequestParamInterface.php
+++ src/Kernel/Interface/RequestParamInterface.php
@@ -5,10 +5,4 @@ namespace AdOceanSdk\Kernel\Interface;
 interface RequestParamInterface
 {
     public function toArray(): array;
-
-    public function messages(): array;
-
-    public function rules(): array;
-
-    public function validate(): void;
 }
