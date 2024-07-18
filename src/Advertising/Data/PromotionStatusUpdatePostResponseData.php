889768c428cd962a96ead0819c077cddb7d907ae
jsogn <jbanxian@gmail.com>
2024-07-18 11:17:41 +0800

    修复bug

--- src/Advertising/Data/PromotionStatusUpdatePostResponseData.php
+++ src/Advertising/Data/PromotionStatusUpdatePostResponseData.php
@@ -13,12 +13,12 @@ class PromotionStatusUpdatePostResponseData extends Data
     /**
      * @var array<int> $promotion_ids 更新成功的广告ID集合
      */
-    public array $promotion_ids;
-    
+    public array $promotion_ids = [];
+
     /**
      * @var array<\AdOceanSdk\Advertising\Data\PromotionStatusUpdatePostResponseErrorsData> $errors 更新失败的广告列表
      */
     #[\ClassTransformer\Attributes\ConvertArray(\AdOceanSdk\Advertising\Data\PromotionStatusUpdatePostResponseErrorsData::class)]
     public array $errors;
-    
+
 }
