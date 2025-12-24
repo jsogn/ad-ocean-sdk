7c1e1ac59a434d00315beaaa94c9f47e3d7a8d06
Jhon_Walker <jbanxian@gmail.com>
2025-12-24 11:19:17 +0800

    优化代码

--- src/File/Data/FileVideoUploadTaskListGetResponseListData.php
+++ src/File/Data/FileVideoUploadTaskListGetResponseListData.php
@@ -31,7 +31,7 @@ class FileVideoUploadTaskListGetResponseListData extends Data
     public int $task_id;
     
     /**
-     * @var ?\AdOceanSdk\File\Data\FileVideoUploadTaskListGetResponseListVideoInfoData $video_info 视频素材信息
+     * @var \AdOceanSdk\File\Data\FileVideoUploadTaskListGetResponseListVideoInfoData|null $video_info 视频素材信息
      */
     public ?\AdOceanSdk\File\Data\FileVideoUploadTaskListGetResponseListVideoInfoData $video_info;
     
